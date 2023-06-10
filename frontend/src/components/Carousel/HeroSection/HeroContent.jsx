41818b2148c03b4d1fe9b939d58a28b30fb1cf66
MarcGames1 <George.marcu20@gmail.com>
2023-06-10 08:14:46 +0300

    Update HeroContent.jsx

--- frontend/src/components/Carousel/HeroSection/HeroContent.jsx
+++ frontend/src/components/Carousel/HeroSection/HeroContent.jsx
@@ -7,8 +7,8 @@ import { Hero, Artboard, Button } from 'react-daisyui';
 const HeroContent = ({titlu = <></>, subtitlu=<></>, svg =<></>}) => {
 
   return (
-    <div className="min-h-96 pl-20	leading-loose flex bg-base-100 z-10 justify-items-center w-full  justify-center gap-4">
-      <Hero className="flex-0 z-10">
+    <div className="min-h-96 pl-20 lg:flex-row flex-col	leading-loose flex bg-base-100 z-10 justify-items-center w-full  justify-center gap-4">
+      <Hero className=" z-10">
         <Hero.Overlay className=" z-10 bg-opacity-60" />
         <Hero.Content className=" z-10 bg-base-100 text-left">
           <div className="max-w z-10">
@@ -21,7 +21,7 @@ const HeroContent = ({titlu = <></>, subtitlu=<></>, svg =<></>}) => {
           </div>
         </Hero.Content>
       </Hero>
-      <div className=" z-10 w-full relative">{svg}</div>
+      <div className=" z-10 w-full h-full relative">{svg}</div>
     </div>
   );
 }
