1be7f271bb65eccb49a89a04591de1cb51a15e07
MarcGames1 <George.marcu20@gmail.com>
2023-06-30 13:35:47 +0300

    x

--- frontend/src/components/IconAndText/IconAndText.jsx
+++ frontend/src/components/IconAndText/IconAndText.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const IconAndText = ({icon, heading, p, button}) => {
   return (
-    <div className=" flex sm:w-full md:w-full w-3/5 p-3 gap-3 justify-around m-auto items-center flex-col md:flex-row lg:flex-row ">
+    <div className=" flex sm:w-full md:w-full w-4/5  p-3 gap-3 justify-around m-auto items-center flex-col md:flex-row lg:flex-row ">
       <div className=" max-w-sm ">{icon}</div>
       <div className="w-full prose lg:p-4 flex-end flex-1">
         <h2>{heading}</h2>
