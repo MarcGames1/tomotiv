198225680c2b3ffc005d5958ef2df4bd68899bd8
MarcGames1 <George.marcu20@gmail.com>
2023-07-05 16:21:32 +0300

    Update CoursesCardsLoader.jsx

--- frontend/src/app/admin/cursuri/componenteAdministrareCurs/CoursesCardsLoader.jsx
+++ frontend/src/app/admin/cursuri/componenteAdministrareCurs/CoursesCardsLoader.jsx
@@ -1,29 +1,37 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ApiClient from '@/Classes/ApiClient';
 import CourseCard from '../../adminComponents/CourseCard';
 const apiPath = process.env.NEXT_PUBLIC_API;
 const api = new ApiClient(apiPath);
-const apiP = `${process.env.NEXT_PUBLIC_API}/courses`
 
 export const revalidate = 10;
 
 const getCoursesList = async () =>{
   try{
-    const courses = await fetch(apiP, { next: { revalidate: 60 } });
+    const courses = await api.get('/courses');
     if (!courses) {
       throw new Error('Failed to fetch data');
     }
     console.log(courses)
-    return courses.json()
+    return courses
   } catch(err) {
     console.log(err);
   }
 }
-const CoursesCardsLoader = async () => {
+const CoursesCardsLoader =  () => {
 
-    const courses = await getCoursesList()
-    
+    const [courses, setCourses] = useState([])
+
+    useEffect(()=>{
+      if(courses.length === 0){
+      const updateCourses = async () =>{
+        setCourses(await getCoursesList());
+      }
+      updateCourses();
+      }
+    },[])
+  
 
 
       return (
