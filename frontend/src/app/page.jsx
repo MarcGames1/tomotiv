b39ab25204ac560c346c8599530b3ea57f513ee0
MarcGames1 <George.marcu20@gmail.com>
2023-08-10 09:51:36 +0300

    Update page.jsx

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -144,18 +144,18 @@ const MembruEchipa = ({nume, prenume, poza, specializare, functie, contact}) =>{
     <>
       <div className=" w-full justify-center m-auto text-center">
         <Avatar alt={`poza ${prenume} ${nume} ${functie}`} src={poza} />
-        <div className=" text-center ">
+        <div className="flex flex-col m-auto text-center ">
           <h5 className="text-center font-extrabold my-3 ">
             {prenume} {nume}
           </h5>
           <p className="font-bold my-3">{functie}</p>
-          <p>{specializare}</p>
+          <p className=" mx-10">{specializare}</p>
           <div className="my-3">
             <Link
               href={`/echipa/${prenume.toLowerCase()}-${nume.toLowerCase()}`}
             >
               {' '}
-              <button className="btn btn-primary">Afla mai mult</button>
+              <button className="btn btn-secondary">Afla mai mult</button>
             </Link>
           </div>
         </div>
