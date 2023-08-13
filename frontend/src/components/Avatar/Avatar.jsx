e630f031bb2b0638b5d5ee7ae6b4e4aece5e3e9e
MarcGames1 <George.marcu20@gmail.com>
2023-08-13 22:39:58 +0300

    Update Avatar.jsx

--- frontend/src/components/Avatar/Avatar.jsx
+++ frontend/src/components/Avatar/Avatar.jsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
+
 const Avatar = ({ src, alt }) => {
   return (
     <>
       <div className="avatar m-auto w-full block">
-        <div className="h-1/2 my-10 w-1/2 m-auto block">
-          <img src={src} style={{ objectFit: 'cover' }} alt={alt} />
+        <div className="relative h-1/2 my-10 overflow-clip w-1/2 m-auto block">
+          <img src={src} loading="lazy" alt={alt} />
         </div>
       </div>
     </>
